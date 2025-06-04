b194fa509bb6d3a39592dc411d998d10c5e1dc66
Eduard1151456 <eduardmauriciolh@ufps.edu.co>
2025-06-04 03:22:55 -0500

    Progreso en API de productos

--- backend-supabase/routes/productos.js
+++ backend-supabase/routes/productos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const supabase = require('./supabaseClient'); // Importa tu cliente supabase
+const supabase = require('../supabaseClient'); // Importa tu cliente supabase
 
 // Get all products
 router.get('/', async (req, res) => {
--- backend-supabase/supabaseClient.js
+++ backend-supabase/supabaseClient.js
@@ -1,9 +1,9 @@
-import 'dotenv/config';
-import { createClient } from '@supabase/supabase-js';
+require('dotenv').config();
+const { createClient } = require('@supabase/supabase-js');
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_KEY;
 
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase;
+module.exports = supabase;
